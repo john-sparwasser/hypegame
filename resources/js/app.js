6b51da43e0b512facee204deab2a08f0d57da0c1
John Sparwasser <johnspar1@gmail.com>
2017-06-05 08:48:45 -0400

    Vue done

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,4 +1,36 @@
-Vue.component('release')
+Vue.component('release', {
+    props: ['hype', 'name', 'description', 'image', 'hypable'],
+
+    template: '' +
+        '<div class="release">' +
+            '<h1><span v-on:click="incrementHype" class="hype">🔥</span> {{ releaseHype }}</h1>' +
+            '<img :src="image" :alt="releaseName" />' +
+            '<h2>{{ releaseName }}</h2>' +
+            '<p>{{ releaseDescription }}</p>' +
+            '<div class="separator"></div>' +
+        '</div>',
+
+    methods: {
+
+        incrementHype() {
+            if (this.releaseHypable) {
+                this.releaseHype++
+                this.releaseHypable = false
+            }
+        }
+
+    },
+
+    data() {
+        return {
+            releaseHype: this.hype,
+            releaseName: this.name,
+            releaseDescription: this.description,
+            releaseImage: this.image,
+            releaseHypable: this.hypable
+        }
+    }
+})
 
 new Vue({
     el: '#hypegame',
