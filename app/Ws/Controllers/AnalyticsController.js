dac294387f5efd993c5973f841e01418422062f6
John Sparwasser <johnspar1@gmail.com>
2017-06-08 15:24:45 -0400

    Correct base 64 encoding

--- app/Ws/Controllers/AnalyticsController.js
+++ app/Ws/Controllers/AnalyticsController.js
@@ -11,7 +11,7 @@ class AnalyticsController {
 
     onMessage(message) {
         const payload = {
-            Data: Buffer.from(message, 'base64'),
+            Data: new Buffer(message).toString('base64'),
             StreamName: 'hypegame',
             PartitionKey: 'analytics'
         }
